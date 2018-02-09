a1ffbefd917389d1e896dfb9be16a98e6ce664e1
Jorden <jlemarr62@gmail.com>
2018-02-08 23:03:50 -0800

    Just doing homework

---
+++ HW-3/Hello-world2/sketch.js
@@ -0,0 +1,14 @@
+//declare the setup () function
+function setup (){
+  //create a 800 X 300 canvas
+  createCanvas (800, 300)
+
+  //Color the canvas 'indigo'
+  background ('indigo')
+}
+
+//declare the draw () function
+function draw (){
+  //using the draw function, tell p5 to write "hello world!"
+  text ("Hello World!", 50, 250);
+}
