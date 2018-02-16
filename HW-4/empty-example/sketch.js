bc2644592449183289163ee6a0241b373c7d4c58
Jorden <jlemarr62@gmail.com>
2018-02-16 01:01:35 -0800

    actually fixing color

--- HW-4/empty-example/sketch.js
+++ HW-4/empty-example/sketch.js
@@ -5,7 +5,7 @@ function setup () {
 
 //setup draw function and color canvas
 function draw () {
-  background ( rgb(125, 29, 186) );
+  background ( 'rgb(125, 29, 186)' );
 
   //main sandbox
   push();
@@ -16,7 +16,7 @@ Translate(300, 200);
 //body
 push();
 nostroke();
-fill( rgb(119, 8, 8) );
+fill( 'rgb(119, 8, 8)' );
 rect(-50, 75, 100, 250);
 
   pop();
